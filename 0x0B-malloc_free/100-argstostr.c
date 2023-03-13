3075304b32f3a7192925b9cf63d2784876ec11dc
belledame1047 <eniolakayode28@gmail.com>
2023-03-13 14:55:16 -0700

    malloc

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+#include <stdlib.h>
+
+/**
+ * argstostr - main entry
+ *
+ * @ac: int input
+ *
+ * @av: double pointer array
+ *
+ * Return: 0
+ *
+*/
+
+char *argstostr(int ac, char **av)
+
+{
+
+	char *str, *s;
+	int i, j, k, len = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		s = av[i];
+		j = 0;
+
+		while (s[j++])
+			len++;
+
+		len++;
+	}
+
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0, j = 0; i < ac && j < len; i++)
+	{
+		s = av[i];
+		k = 0;
+
+		while (s[k])
+		{
+			str[j] = s[k];
+			k++;
+			j++;
+		}
+		str[j++] = '\n';
+	}
+
+	str[j] = '\0';
+
+	return (str);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,14 @@
+0x0B-malloc_free
+TASK 0. function that creates an array of chars, and initializes it with a specific char.
+
+TASK 1. A function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.
+
+TASK 2. A function that concatenates two strings.
+
+TASK 3. A function that returns a pointer to a 2 dimensional array of integers.
+
+TASK 4. A function that frees a 2 dimensional grid previously created by your alloc_grid function.
+
+TASK 5. A function that concatenates all the arguments of your program.
+
+TASK 6. A function that splits a string into words.

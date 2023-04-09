97251ec583109662a52c462b966f230275a915b1
enniebaby <eniolakayode28.gmail.com>
2023-04-09 14:53:35 -0700

    near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+/**
+ * File: 0-read_textfile.c
+ * Author: Belledame.
+ */
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters the function is to read and print
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	ssize_t o;
+	ssize_t w;
+	ssize_t r;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+	o = open(filename, O_RDONLY);
+	r  = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
+	free(buffer);
+	close(o);
+	return (w);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,16 @@
+0x15. C - File I/O
+
+Task 0: Tread lightly, she is near
+	A function that reads a text file and prints it to the POSIX standard output.
+
+Task 1: Under the snow
+	A function that creates a file.
+
+Task 2: Speak gently, she can hear
+	A function that appends text at the end of a file.
+
+Task 3: CP
+	A program that copies the content of a file to another file.
+
+Task 4: Elf
+	A program that displays the information contained in the ELF header at the start of an ELF file.
